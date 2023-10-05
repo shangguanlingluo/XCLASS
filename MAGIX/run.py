c738c126ad267a7e34e3df7875a76cefb8dab47b
shangguanlingluo <blsmlh@163.com>
2023-10-05 15:35:05 +0800

    Python code to execute calculate

---
+++ MAGIX/run.py
@@ -0,0 +1,6 @@
+MolfitsFileName = "molecule.molfit"
+experimentalData = "observation.xml"
+AlgorithmXMLFile = "algorithm-settings.xml"
+RestFreq = 0.0
+vLSR = 0.0
+newmolfit , modeldata , JobDir = myXCLASSFit ()
